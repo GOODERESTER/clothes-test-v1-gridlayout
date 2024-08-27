b4e96ab9740c6624c6545ccd7c9d3a890a598bfb
GOODERESTER <66927822+GOODERESTER@users.noreply.github.com>
2024-08-26 22:21:05 -0400

    fixed problem

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,22 @@
 import { defineStorage } from "@aws-amplify/backend";
 
+// export const clothesInfo = defineStorage({
+//   name:'clothes',
+//   access: (allow) => ({
+//     ''
+//   })
+// })
+
 export const storage = defineStorage({
   name: "amplifyNotesDrive",
   access: (allow) => ({
-    "media/{entity_id}/*": [
-      allow.entity("identity").to(["read", "write", "delete"]),
+    'profile-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
     ],
-  }),
+    'picture-submissions/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+  })
 });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import outputs from 'amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
