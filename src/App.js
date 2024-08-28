1b90431b4a054006154be45f2539b0bd78cbc5b0
GOODERESTER <66927822+GOODERESTER@users.noreply.github.com>
2024-08-28 17:17:17 -0400

    Made Amplify work again

--- src/App.js
+++ src/App.js
@@ -10,6 +10,29 @@
 // import Reviews from './components/reviews/Reviews';
 // import Search from './components/search/Search'
 // import Detailed from './components/detailed/Detailed';
+// import { Amplify } from "aws-amplify";
+// import "@aws-amplify/ui-react/styles.css";
+// import { getUrl } from "aws-amplify/storage";
+// import { uploadData } from "aws-amplify/storage";
+// import { generateClient } from "aws-amplify/data";
+// import {
+//   Authenticator,
+//   Button,
+//   Text,
+//   TextField,
+//   Heading,
+//   Flex,
+//   View,
+//   Image,
+//   Grid,
+//   Divider,
+// } from "@aws-amplify/ui-react";
+// import outputs from "amplify_outputs.json";
+
+// Amplify.configure(outputs);
+// const client = generateClient({
+//   authMode: "userPool",
+// });
 
 // function App() {
 
@@ -17,6 +40,66 @@
 //   const [clothing, setClothing] = useState();
 //   const [reviews, setReviews] = useState([]);
 //   const [queryResponse, setQuery] = useState([]);
+//   const [notes, setNotes] = useState([]);
+
+//   useEffect(() => {
+//     fetchNotes();
+//   }, []);
+
+//   async function fetchNotes() {
+//     const { data: notes } = await client.models.Note.list();
+//     await Promise.all(
+//       notes.map(async (note) => {
+//         if (note.image) {
+//           const linkToStorageFile = await getUrl({
+//             path: ({ identityId }) => `media/${identityId}/${note.image}`,
+//           });
+//           console.log(linkToStorageFile.url);
+//           note.image = linkToStorageFile.url;
+//         }
+//         return note;
+//       })
+//     );
+//     console.log(notes);
+//     setNotes(notes);
+//   }
+
+//   async function createNote(event) {
+//     event.preventDefault();
+//     const form = new FormData(event.target);
+//     console.log(form.get("image").name);
+
+//     const { data: newNote } = await client.models.Note.create({
+//       name: form.get("name"),
+//       description: form.get("description"),
+//       image: form.get("image").name,
+//     });
+
+//     console.log(newNote);
+//     if (newNote.image)
+//       if (newNote.image)
+//         await uploadData({
+//           path: ({ identityId }) => `media/${identityId}/${newNote.image}`,
+
+//           data: form.get("image"),
+//         }).result;
+
+//     fetchNotes();
+//     event.target.reset();
+//   }
+
+//   async function deleteNote({ id }) {
+//     const toBeDeletedNote = {
+//       id: id,
+//     };
+
+//     const { data: deletedNote } = await client.models.Note.delete(
+//       toBeDeletedNote
+//     );
+//     console.log(deletedNote);
+
+//     fetchNotes();
+//   }
 
 //   const getClothes = async () =>{
 //     try{
@@ -108,9 +191,9 @@ import "@aws-amplify/ui-react/styles.css";
 import { getUrl } from "aws-amplify/storage";
 import { uploadData } from "aws-amplify/storage";
 import { generateClient } from "aws-amplify/data";
-import outputs from "amplify_outputs.json";
+import outputs from "./amplify/amplify_outputs.json";
 /**
- * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ * @type {import('aws-amplify/data').Client<import('./amplify/data/resource').Schema>}
  */
 
 Amplify.configure(outputs);
